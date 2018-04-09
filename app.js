28d1c1c17174787afbe9d0b306ec1d0470282549
Michael Herman <hermanmu@gmail.com>
2018-04-09 02:41:29 -0600

    added cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.get('/ping', (req, res, next)  => {
   res.status(200).json('pong!');
